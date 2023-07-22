80e1ad976ce645ceb2af3247448ee1b750e5fe12
Nicotelo <140250412+Nicotelo@users.noreply.github.com>
2023-07-22 14:36:10 +0200

    renames

---
+++ 02_DrawingAShape.rb
@@ -0,0 +1,21 @@
+#print "Ho ho hoooo"
+=begin
+def puts2 string
+    print "#{string}\n"
+end
+
+puts2 "Ho ho hooooo"
+puts2 "Holaito"
+=end
+
+# In this exercise we are drawing a basic triangle shape
+# puts work fine, but I wanted to create my own puts. Its the same, just felt like using trying to create basic functions on my own
+def puts2 string
+    print "#{string}\n"
+end
+
+puts2 "    /|"
+puts2 "   / |"
+puts2 "  /  |"
+puts2 " /   |"
+puts2 "/____|"
--- DrawingAShape.rb
+++
@@ -1,1 +0,0 @@
-print "Ho ho hoooo"
