fd95d487fa627acdd9cf7cfbb66a747a15697f47
Nicotelo <140250412+Nicotelo@users.noreply.github.com>
2023-07-22 15:08:21 +0200

    RenamedAgain

---
+++ 04_Maths.rb
@@ -0,0 +1,25 @@
+#https://www.tutorialspoint.com/ruby/ruby_operators.htm
+
+puts 5
+puts 5.86543
+puts -5.86543
+puts 5+9
+puts 5-9
+puts 5*9
+puts 5/9
+puts 5/9.0
+puts 5%9
+# puts 2^3 --> creo que esto es binario
+puts 2**3
+
+num = 20
+puts ("Este es mi número " + num.to_s) # hay que pasarlo a string con este modo
+puts "Este es mi número #{num}"
+=begin
+ .abs()  --> dará el valor absoluto del número
+ .round() --> redondea el número
+ .ceil() --> redondea hacia arriba
+ .floor() --> redondea hacia abajo
+ Math.sqrt(num) --> nos dará la raíz cuadrada
+ Math.log(num) --> logaritmo
+=end
